bb04d5845cd50c3f678ae04584ae810d0a40a334
Oh Jisu <parangwavetraveller@gmail.com>
2024-09-06 21:54:25 +0900

    Practice Project - Patterns

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Seo from "../components/Seo";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Seo title="Home" />
       <h1 className="active">Hello</h1>
     </div>
   );
